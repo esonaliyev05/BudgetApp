40d97eddb8c47b44a02f6f6e6d58bf25a507acc0
Alyorbekjonxon <alyorbek391@gmail.com>
2025-03-04 13:56:32 +0500

    new

--- Data/Context/AppDbContext.cs
+++ Data/Context/AppDbContext.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-
 namespace BudgetApp.Data.Context;
 
 public class AppDbContext : IdentityDbContext<User, IdentityRole<Guid>, Guid>
